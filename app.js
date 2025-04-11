67ed642accc7ab379e7df302de90a3f0587a008c
MR-Titus02 <titusroxsan@gmail.com>
2025-04-11 14:25:44 +0530

    hqwd

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
- let field = document.getElementById("field").value;
-  let taskList = document.getElementById("taskList");
-  let list = document.createElement("div");
+ 
   let inpvalue = document.getElementsByTagName("input")
 function myFunction() {
-  
+    let field = document.getElementById("field").value;
+    let taskList = document.getElementById("taskList");
+    let list = document.createElement("div");
   list.innerHTML = `<span class="lists">${field}</span>` + `<input type="checkbox">`;
   taskList.appendChild(list);
 }
 function done() {
-    if
+    
 }
